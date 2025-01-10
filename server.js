0f9e50e1267c345f5c20bed2af8f3037bfd3593f
cdeorta-dev <cdeorta.dev@gmail.com>
2025-01-10 13:43:15 -0300

    modificacion de server

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.get('/navidad', (req, res) => {
-    res.send('feliz navidad!!! ♥')
-  })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
